115cfe7f34041ad80175450307f2e78015dbeb6b
sammun1926 <munorusam10@gmail.com>
2023-03-25 08:12:17 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+	e.preventDefault();
+	// get data from the submitted form
+	const myForm = document.getElementById("create-task-form");
+	myForm.addEventListener("submit", (e) => {
+		// Prevent the default Value
+		e.preventDefault();
+		const newTask = document.getElementById("new-task-description");
+		const taskList = document.getElementById("tasks");
+		const newTaskItem = document.createElement("li");
+		newTaskItem.innerText = newTask.value;
+		taskList.appendChild(newTaskItem);
+		// Reset the dom
+		e.target.reset();
+	});
 });
